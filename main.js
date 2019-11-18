bbb115de28180d1a2dae5a8a1097391319c45f3d
Kémici Cagnard <kemici.cagnard@outlook.com>
2019-11-18 17:26:06 +0100

    Ajout du JS

--- main.js
+++ main.js
@@ -22,11 +22,11 @@ window.onclick = function(event) {
 
 // Mes variables
 
-    // Tableau qui va enregistrer la couleur qu'on a générée aléatoirement
-    let randomColor = []
+    // Tableau qui va stocker la couleur qu'on a générée aléatoirement
+    let randomColorStock = []
 
-    // Tableau qui va enregistrer les couleurs sur lesquelles l'utilisateur a cliqué
-    let colorClick = []
+    // Tableau qui va stocker les couleurs sur lesquelles l'utilisateur a cliqué
+    let clickColorStock = []
 
 // Lancement du jeu
 
@@ -37,55 +37,59 @@ function start(){
 // Reset de la partie
 
 function reset(){
-    randomColor = []
-    colorClick = []
+    randomColorStock = []
+    clickColorStock = []
     
-    console.log(randomColor, colorClick)
+    console.log(randomColorStock, clickColorStock)
     
 }
 
 // Si c'est au joueur ou à l'IA de jouer
 
 
-// Fonction générant un nombre aléatoire entre 0 et 3 pour l'utiliser plus tard
+// Variable générant un nombre aléatoire entre 0 et 3 pour l'utiliser plus tard
 
-function randomNumber() {
-    return Math.floor(Math.random() * 4); 
-}
+let randomNumber =  Math.floor(Math.random() * 4); 
 
-console.log(randomNumber())
-// On va associer le nombre random que l'on vient de récupérer à un bouton
 
-switch (randomNumber()) {
+console.log(randomNumber)
+
+// On va associer le nombre random que l'on vient de créer à un bouton
+
+switch (randomNumber) {
     case 0:
         document.getElementById(0)
-        randomColor.push(this.id)
+        randomColorStock.push(this.id)
         console.log('Vert')
     break
     case 1:
         document.getElementById(1)
-        randomColor.push(this.id)
+        randomColorStock.push(this.id)
         console.log('Rouge')
     break
     case 2:
         document.getElementById(2)
-        randomColor.push(this.id)
+        randomColorStock.push(this.id)
         console.log('Bleu')
     break
     case 3:
         document.getElementById(3)
-        randomColor.push(this.id)
+        randomColorStock.push(this.id)
         console.log('Jaune')
     break
 }
 
-console.log(randomColor)
+console.log(randomColorStock)
 
 // Quand on clique sur un bouton, on va push son ID dans l'array colorClick
 
 function clickOnColor(id){
-    colorClick.push(id)
-    console.log(colorClick)
+    clickColorStock.push(id)
+    console.log(clickColorStock)
 }
 
 // On check que l'array randomColor qui génère aléatoirement la suite soit bien raccord avec colorClick, où l'on a reproduit cette suite 
+
+if (randomColorStock == clickColorStock) {
+    console.log('Bonne réponse')
+}
